cc8b61b2cb7aa2026baca42d8177aa80939c3201
Susana Figueroa <99418807+susanafigueroa@users.noreply.github.com>
2022-02-10 21:37:38 +0100

    oracle_ejercicio3
    que pida ingresar el radio de la circunferencia y muestre su área.

---
+++ PLSQL/oracle/oracle_ejercicio3.sql
@@ -0,0 +1,14 @@
+SET SERVEROUTPUT ON;
+
+DECLARE
+
+radio number(3,1);
+pi constant number(3,2) := 3.14;
+
+BEGIN
+
+radio := &radio;
+
+DBMS_OUTPUT.PUT_LINE('el radio de la circumferencia es : ' || radio || ' y el área es ' ||pi*(radio*radio));
+
+END;
