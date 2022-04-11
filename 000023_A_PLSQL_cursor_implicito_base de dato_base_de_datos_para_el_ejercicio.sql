2370576ef43a65280f1efe75b3b1c83cae3bf620
Susana Figueroa <99418807+susanafigueroa@users.noreply.github.com>
2022-04-11 11:21:28 +0200

    database exercise 000023_A

---
+++ 000023_A_PLSQL_cursor_implicito_base de dato_base_de_datos_para_el_ejercicio.sql
@@ -0,0 +1,16 @@
+drop table empleados;
+
+create table empleados(
+    documento char(8),
+    apellido varchar2(30),
+    nombre varchar2(30),
+    domicilio varchar2(20),
+    oficio varchar2(20),
+    sueldo number(8,2)
+);
+
+ insert into empleados values('22222222','Acosta','Ana','Avellaneda 11','Secretaria',1800);
+ insert into empleados values('23333333','Bustos','Betina','Bulnes 22','Gerencia',5000);
+ insert into empleados values('24444444','Caseres','Carlos','Colon 333','Contabilidad',3000);
+ insert into empleados values('32323255','Gonzales','Miguel','Calle 4ta No.90','Contabilidad',8000);
+ insert into empleados values('56565555','Suarez','Tomas','Atarazana 78','Cobros',1500);
