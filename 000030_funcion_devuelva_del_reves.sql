6d49983462d98eb1764c3d5e03d774b5435ea738
Susana Figueroa <99418807+susanafigueroa@users.noreply.github.com>
2022-04-18 20:26:37 +0200

    Add files via upload

---
+++ 000030_funcion_devuelva_del_reves.sql
@@ -0,0 +1,63 @@
+/* 1- Realizar una función llamada DelReves que a partir de un codigo
+de producto, nos devuelva el código de producto al revés.
+Utilizar en una consulta de ejemplo para comprobar que funciona.
+
+/*Ejemplo de función:
+create or replace function Pagos_cliente(v_codigocliente clientes.codigocliente%type)
+return Number
+as
+  v_sumapagos pagos.cantidad%type := 0;
+begin
+   
+  select sum(cantidad) into v_sumapagos
+  from pagos
+  where codigocliente = v_codigocliente;
+ 
+  return v_sumapagos;
+ 
+end;
+/
+
+/*Ejemplo de llamada de la función:
+select Pagos_cliente(3) as Pagos_cliente_3 
+from dual;
+
+
+
+
+
+
+/*RESOLUCIÓN:*/
+
+create or replace function DelReves(codigoUser jardineria_producto.codigo_producto%TYPE) 
+return jardineria_producto.codigo_producto%TYPE
+as
+
+largadaCodigoUser number(5);
+codigoUserReves jardineria_producto.codigo_producto%TYPE;
+
+begin
+
+largadaCodigoUser:=length(codigoUser);
+
+for i in reverse 1..largadaCodigoUser loop--el bucle da tantas vueltas como dígitos tiene el codigoUser
+
+    codigoUserReves:=codigoUserReves || substr(codigoUser,i,1);--necesito usar substr porque tengo que pillar cada letra de codigoUser
+--en cada vuelta del bucle se van añadiendo las letras sueltas de codigoUser a codigoUserReves
+
+end loop;
+
+    return codigoUserReves;
+
+end;
+/
+
+
+
+
+
+
+/*EJEMPLO PARA VER SI FUNCIONA O NO*/
+
+select DelReves('FR-100') from dual;
+
