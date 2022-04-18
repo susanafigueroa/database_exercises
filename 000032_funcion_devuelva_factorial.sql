9e748c755c7506d056241033548e058ccd3f381d
Susana Figueroa <99418807+susanafigueroa@users.noreply.github.com>
2022-04-18 20:36:41 +0200

    database exercise 000032

--- 000032_funcion_devuelva_factorial.sql
+++ 000032_funcion_devuelva_factorial.sql
@@ -1,34 +1,7 @@
-/*5 - Programar una función que calcule el factorial de un numeroq ue se le pase
-como parametro. La función se llamará factorial.*/
+/*5 - Programar una funciÃ³n que calcule el factorial de un numeroq ue se le pase
+como parametro. La funciÃ³n se llamarÃ¡ factorial.*/
 
-/*Ejemplo de función:
-create or replace function Pagos_cliente(v_codigocliente clientes.codigocliente%type)
-return Number
-as
-  v_sumapagos pagos.cantidad%type := 0;
-begin
-   
-  select sum(cantidad) into v_sumapagos
-  from pagos
-  where codigocliente = v_codigocliente;
- 
-  return v_sumapagos;
- 
-end;
-/
-
-/*Ejemplo de llamada de la función:
-select Pagos_cliente(3) as Pagos_cliente_3 
-from dual;
-
-
-/*el for factorial en java:*/
-/*for ( int i = number; i > 1; i--){
-fact = fact * i;
-}*/
-
-
-/*RESOLUCIÓN:*/
+/*RESOLUCIÃ“N:*/
 create or replace function Factorial(numeroIntroducido number)
 return number
 as
